3b1c061b0d3b23a47095b27af6ad45fad6d8fcc0
unknown <EMartz@BOG082196.bankga.com>
2015-04-16 16:05:02 -0400

    Created application, Products scaffold and ran rake:db migration.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '2865098c62377ac873f291738b00f131699ff2f1c0d7c51826fffa9a45be9f373d474d1063fd03d80959b0faf04e72e31a25c004ec8d9d0df354416316acbc00'
