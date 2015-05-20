764cacf32c26bbae3540342ca7ebef9a3a094fc8
Eric Martz <ericmartz@gmail.com>
2015-05-20 13:03:43 -0400

    Added tests to cart_test.rb.

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "adds an item to the cart" do 
+  	cart = Cart.new
+  	cart.add_product(products(:ruby).id, products(:ruby).price)
+  	assert cart.save
+  end
+
+  test "adds multiple items to the cart" do 
+  	cart = Cart.new
+  	cart.add_product(products(:coffee).id, products(:coffee).price)
+  	cart.add_product(products(:ruby).id, products(:ruby).price)
+  	assert cart.save
+  end
 end
